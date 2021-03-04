4c1cb8796c2b331fb6b65f7c917a6b8b91ca83c5
NysaClark <69279788+NysaClark@users.noreply.github.com>
2021-03-03 18:22:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+`use strict`
+
+$(function(){
+    let interval;
+
+    // the stop watch starts off and if you click the start/stop button it turns it starts or stops
+    let isRunning = false;
+    let $startStop = $(`#startStop`);
+    $startStop.on(`click`, function(){
+        if(isRunning == false){
+            isRunning = true;
+            startWatch();
+            $startStop.html(`STOP`)
+        }else{
+            isRunning = false;
+            stopWatch();
+            $startStop.html(`START`)
+        }
+    })
+
+    // variables to keep track of the time
+    let ms = 0;
+    let sec = 0;
+    let min = 0;
+
+    // function to start the stop watch that increases the time every 100ms
+    function startWatch(){
+        interval = setInterval(function(){
+            ms++;
+
+            if(ms < 10){
+                $(`#ms`).html(`0${ms}`);
+            }
+            if(ms < 100 && ms >= 10){
+                $(`#ms`).html(`${ms}`);
+            }
+            if(ms == 100){
+                ms = 0;
+                sec++;
+            }
+
+            if(sec < 10){
+                $(`#sec`).html(`0${sec}`)
+            }
+            if(sec < 60 && sec >= 10){
+                $(`#sec`).html(`${sec}`);
+            }
+            if(sec == 60){
+                sec = 0;
+                min++;
+            }
+
+            if(min < 10){
+                $(`#min`).html(`0${min}`);
+            }
+            if(min >= 10){
+                $(`#min`).html(`${min}`);
+            }
+        }, 10);
+    }
+
+    //function to stop the watch
+    function stopWatch(){
+        clearInterval(interval);
+    }
+
+    //lap button onclick function
+    let $lap = $(`#lap`);
+    $lap.on(`click`, function(){
+        if(ms < 10){
+            $(`#lapMs`).html(`0${ms}`);
+        }
+        if(ms >= 10){
+            $(`#lapMs`).html(`${ms}`);
+        }
+
+        if(sec < 10){
+            $(`#lapSec`).html(`0${sec}`)
+        }
+        if(sec >= 10){
+            $(`#lapSec`).html(`${sec}`);
+        }
+
+        if(min < 10){
+            $(`#lapMin`).html(`0${min}`);
+        }
+        if(min >= 10){
+            $(`#lapMin`).html(`${min}`);
+        }
+    })
+
+})
